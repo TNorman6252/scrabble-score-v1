88477a30d3f85cc2479d396cea53d4f7371ee378
Tristan <tristan6252@gmail.com>
2022-02-10 23:10:04 +0000

    change: scoreLetter method in ScrabbleScore class to uppercaseChar when testing with when

--- src/main/kotlin/ScrabbleScore.kt
+++ src/main/kotlin/ScrabbleScore.kt
@@ -12,7 +12,7 @@ class ScrabbleScore {
     //TODO the program if the user enters in any chars other than capital letters...try fix this.
     //Reference: https://exercism.org/tracks/kotlin/exercises/scrabble-score/solutions?page=1
     private fun scoreLetter(char: Char): Int {
-        return when (char) {
+        return when (char.uppercaseChar()) {
             in "AEIOULNRST" -> 1
             in "DG" -> 2
             in "BCMP" -> 3
